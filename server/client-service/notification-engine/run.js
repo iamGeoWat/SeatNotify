0e2a792384ab73063836e9445db1279ae3fb8bb7
geowat <ldsxxs@163.com>
2020-03-08 19:02:18 +0800

    remove files

--- server/client-service/notification-engine/run.js
+++ server/client-service/notification-engine/run.js
@@ -8,81 +8,85 @@ const subscriptionDao = new SubscriptionDao();
 const wxConf = require('../config/wxConfig');
 
 subRedis.subscribe("update_timestamp", (err, count) => {
-  console.log("err", err);
-  console.log("count", count);
+  console.log("err:", err);
+  console.log("count:", count);
   subRedis.on("message", async (channel, message) => {
-    console.log("c", channel);
-    console.log("m", message);
+    console.log("channel:", channel);
+    console.log("msg:", message);
     if (message === '') {
       console.log('info core test days loading error.')
     } else {
       actRedis.get('seat').then(async (records) => {
         //--------- prepare subscribe data in DB --------- OUTPUT: { city@date: itsSubscribers }
-        let hotSubs = await subscriptionDao.queryHotSubs(); // todo: 处理query为空的情况（没有订阅时）
-        console.log(hotSubs);
-        let cityAtDateAndItsSubscribers = {}; //这样更简单，但是把前后端所有cn改成en更好
-        let subscribedCityAtDate = new Set();
-        for (let hotSub of hotSubs) {
-          console.log(hotSub['city']);
-          subscribedCityAtDate.add(hotSub['city']+'@'+hotSub['date'])
-        }
-        for (let cityAtDate of subscribedCityAtDate) {
-          cityAtDateAndItsSubscribers[cityAtDate] = []
-        }
-        for (let hotSub of hotSubs) {
-          cityAtDateAndItsSubscribers[hotSub['city']+'@'+hotSub['date']].push(hotSub['uid'])
-        }
-        console.log(subscribedCityAtDate);
-        console.log(cityAtDateAndItsSubscribers);
-        
-        //---------- prepare seat status data in REDIS ------- OUTPUT: { city@date: how-many-seats-left }
-        let parsedRecords = eval(records);
-        let cityAtDateAndSeatNum = {}; //seat num is the num of test centers that have seats
-        for (let cityAtDate of subscribedCityAtDate) {
-          cityAtDateAndSeatNum[cityAtDate] = 0
-        }
-        for (let record of parsedRecords) {
-          if (subscribedCityAtDate.has(record['cityCn']+'@'+record['date'])) {
-            cityAtDateAndSeatNum[record['cityCn']+'@'+record['date']] += record['seatStatus']
+        let hotSubs = await subscriptionDao.queryHotSubs();
+        if (hotSubs.length === 0) {
+          console.log('No db record in seat_service')
+        } else {
+          console.log(hotSubs);
+          let cityAtDateAndItsSubscribers = {}; //这样更简单，但是把前后端所有cn改成en更好
+          let subscribedCityAtDate = new Set();
+          for (let hotSub of hotSubs) {
+            console.log(hotSub['city']);
+            subscribedCityAtDate.add(hotSub['city']+'@'+hotSub['date'])
           }
-        }
-        console.log(cityAtDateAndSeatNum);
-        
-        //check every city@date from cityAtDateAndItsSubscribers in cityAtDateAndSeatNum, if not 0, mod db::has_seat, do notify, mod db::notified.
-        for (let cityAtDate of subscribedCityAtDate) {
-          console.log(cityAtDate);
-          if (cityAtDateAndSeatNum[cityAtDate] !== 0) {
-            // mod db::has_seat
-            let splitCityAndDate = cityAtDate.split('@');
-            console.log(splitCityAndDate);
-            console.log(cityAtDateAndItsSubscribers[cityAtDate]);
-            for (let uid of cityAtDateAndItsSubscribers[cityAtDate]) {
-              await subscriptionDao.modHasSeatByUidCityDate(1, uid, splitCityAndDate[0], splitCityAndDate[1] )
-              console.log('ACTION: has_seat modified for ', uid)
-            } // mod db::has_seat
-            for (let uid of cityAtDateAndItsSubscribers[cityAtDate]) {
-              console.log('ACTION: sending notification to...', uid);
-              actRedis.get('access_token').then((access_token) => {
-                axios.post(wxConf.subscribeMessageSendUrl + access_token, {
-                  access_token: access_token,
-                  touser: uid,
-                  template_id: wxConf.template_id,
-                  data: {
-                    "thing1": {"value": "托福考位释放"},
-                    "thing3": {"value": splitCityAndDate[0]},
-                    "date5": {"value": splitCityAndDate[1]},
-                    "name4": {"value": "看看考位助手"},
-                    "thing6": {"value": "考位已释放，请前往中国托福官网报名"}
-                  }
-                }).then(async (msg) => {
-                  console.log(msg.data);
-                  if (msg.data.errcode === 0) {
-                    await subscriptionDao.modNotifiedByUidCityDate(1, uid, splitCityAndDate[0], splitCityAndDate[1])
-                    console.log('ACTION: notified modified for ', uid)
-                  }
+          for (let cityAtDate of subscribedCityAtDate) {
+            cityAtDateAndItsSubscribers[cityAtDate] = []
+          }
+          for (let hotSub of hotSubs) {
+            cityAtDateAndItsSubscribers[hotSub['city']+'@'+hotSub['date']].push(hotSub['uid'])
+          }
+          console.log(subscribedCityAtDate);
+          console.log(cityAtDateAndItsSubscribers);
+  
+          //---------- prepare seat status data in REDIS ------- OUTPUT: { city@date: how-many-seats-left }
+          let parsedRecords = eval(records);
+          let cityAtDateAndSeatNum = {}; //seat num is the num of test centers that have seats
+          for (let cityAtDate of subscribedCityAtDate) {
+            cityAtDateAndSeatNum[cityAtDate] = 0
+          }
+          for (let record of parsedRecords) {
+            if (subscribedCityAtDate.has(record['cityCn']+'@'+record['date'])) {
+              cityAtDateAndSeatNum[record['cityCn']+'@'+record['date']] += record['seatStatus']
+            }
+          }
+          console.log(cityAtDateAndSeatNum);
+  
+          //check every city@date from cityAtDateAndItsSubscribers in cityAtDateAndSeatNum, if not 0, mod db::has_seat, do notify, mod db::notified.
+          for (let cityAtDate of subscribedCityAtDate) {
+            console.log(cityAtDate);
+            if (cityAtDateAndSeatNum[cityAtDate] !== 0) {
+              // mod db::has_seat
+              let splitCityAndDate = cityAtDate.split('@');
+              console.log(splitCityAndDate);
+              console.log(cityAtDateAndItsSubscribers[cityAtDate]);
+              for (let uid of cityAtDateAndItsSubscribers[cityAtDate]) {
+                await subscriptionDao.modHasSeatByUidCityDate(1, uid, splitCityAndDate[0], splitCityAndDate[1] )
+                console.log('ACTION: has_seat modified for ', uid)
+              } // mod db::has_seat
+              for (let uid of cityAtDateAndItsSubscribers[cityAtDate]) {
+                console.log('ACTION: sending notification to...', uid);
+                actRedis.get('access_token').then((access_token) => {
+                  axios.post(wxConf.subscribeMessageSendUrl + access_token, {
+                    access_token: access_token,
+                    touser: uid,
+                    template_id: wxConf.template_id,
+                    data: {
+                      "thing1": {"value": "托福考位释放"},
+                      "thing3": {"value": splitCityAndDate[0]},
+                      "date5": {"value": splitCityAndDate[1]},
+                      "name4": {"value": "看看考位助手"},
+                      "thing6": {"value": "考位已释放，请前往中国托福官网报名"}
+                    }
+                  }).then(async (msg) => {
+                    console.log(msg.data);
+                    if (msg.data.errcode === 0) {
+                      await subscriptionDao.modNotifiedByUidCityDate(1, uid, splitCityAndDate[0], splitCityAndDate[1])
+                      console.log('ACTION: notified modified for ', uid)
+                    }
+                  })
                 })
-              })
-            }// send notification, mod db::notified in callback
+              }// send notification, mod db::notified in callback
+            }
           }
         }
       })
