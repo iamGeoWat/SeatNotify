5b6c42969ebfcd2124e5213b566d53bc33f43f2a
geowat <ldsxxs@163.com>
2020-05-13 01:57:45 +0800

    Update index.js

--- server/client-service/routes/index.js
+++ server/client-service/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('GeoWat welcomes you.')
+  res.send('You are connected to a server at my home.')
 });
 
 module.exports = router;
