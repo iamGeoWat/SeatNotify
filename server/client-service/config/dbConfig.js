0164c52f36f8eb1e8d0c78970ba0e189d6377620
liuxikai <liuxikai@meituan.com>
2020-08-04 13:50:50 +0800

    修改参数

--- server/client-service/config/dbConfig.js
+++ server/client-service/config/dbConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   mysql: {
     host: '127.0.0.1',
     user: 'root',
-    password: 'Liuxikai1998',
+    password: 'Liuxikai1998@',
     database: 'seat_service',
     connectionLimit: 10
   }
